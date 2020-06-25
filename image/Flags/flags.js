ba3250cdf4053ef50e02e7b4bff9d2b2bc8b602c
KudosGan <49218397+kudosGan@users.noreply.github.com>
2020-06-25 10:23:57 -0400

    Add files via upload

---
+++ image/Flags/flags.js
@@ -0,0 +1,42 @@
+ {
+    id: 0,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 1,
+    country: "Cabo-Verde",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cabo-Verde.png"
+  },
+  {
+    id: 2,
+    country: "Cameroon",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cameroon.png"
+  },
+  {
+    id: 3,
+    country: "Canada",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Canada.png"
+  },
+  {
+    id: 4,
+    country: "Kosovo",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Kosovo.png"
+  },
+  {
+    id: 5,
+    country: "Liberia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Liberia.png"
+  },
+  {
+    id: 6,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  }
