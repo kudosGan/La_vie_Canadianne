bfc6decc9dfc1fb9349b95d4fa5943a0f851a88a
KudosGan <49218397+kudosGan@users.noreply.github.com>
2020-06-26 09:43:56 -0400

    Update flags.js

--- image/Flags/flags.js
+++ image/Flags/flags.js
@@ -1,4 +1,5 @@
- {
+ [
+  {
     id: 0,
     country: "Cambodia",
     imageUrl:
@@ -36,7 +37,176 @@
   },
   {
     id: 6,
+    country: "Afghanistan",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Afghanistan.png"
+  },
+  {
+    id: 7,
+    country: "Benin",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Benin.png"
+  },
+  {
+    id: 8,
+    country: "Albania",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Albania.png"
+  },
+  {
+    id: 9,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 10,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 11,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 12,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 13,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 14,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 15,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 16,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 17,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 18,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 19,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 20,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 21,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 22,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 23,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 24,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 25,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 26,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 28,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 29,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 30,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 31,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 32,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 33,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 34,
+    country: "Cambodia",
+    imageUrl:
+      "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
+  },
+  {
+    id: 35,
     country: "Cambodia",
     imageUrl:
       "https://kudosgan.github.io/La_vie_Canadianne/image/Flags/flag-of-Cambodia.png"
   }
+]
